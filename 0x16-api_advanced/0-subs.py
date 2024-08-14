2fa11d71a7364a8469a591a6ac19ecfb6e61c4e3
Philip Ajuong Deng <philipajuong@gmail.com>
2024-08-14 18:06:17 +0200

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,7 @@ Task 0 """
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
+
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
