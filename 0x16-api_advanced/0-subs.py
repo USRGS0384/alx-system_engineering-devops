1839bc65d83fe98aa99d411009acfc793ae6e53c
Philip Ajuong Deng <philipajuong@gmail.com>
2024-08-14 19:16:01 +0200

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,21 @@
 """ script to obtain subscribers
     count from a subreddit
 """
-from requests import get
+    subtitle_processor.py
 
+import requests import get
 
+def process_subtitles(subtitles):
 def number_of_subscribers(subreddit):
     """ function to get subscriber count"""
+        subtitle_processor.py
+
     if subreddit and isinstance(subreddit) is str:
         subscribers = 0
         url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
         headers = {'user-agent': 'my-app/0.0.1'}
         req = get(url, headers=headers)
+        response = requests.get(url, timeout=5)
         if req.status_code == 200:
             data = req.json()
             subscribers = data.get('data', {}).get('subscribers', 0)
