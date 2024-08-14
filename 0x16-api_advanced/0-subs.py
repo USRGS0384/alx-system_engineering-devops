a5e578c69a6ce56e617befd4fe9267975b084065
Philip Ajuong Deng <philipajuong@gmail.com>
2024-08-14 19:36:26 +0200

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,8 +13,7 @@ def number_of_subscribers(subreddit):
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    response = requests.get(url, timeout=5)
         return 0
     subscriber_count = response.json().get('data').get('subscribers')
     return subscriber_count
