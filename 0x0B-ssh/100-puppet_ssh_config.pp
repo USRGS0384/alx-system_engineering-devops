e078d6e20c36c3c3bae4555b6afbeba3c963b0a4
Philip Ajuong Deng <philipajuong@gmail.com>
2024-08-22 12:20:39 +0200

    0x0B. SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our cong=figuration file
+file{ 'ect/ssh/ssh_config';
+	ensure => present,
+
+content =>"
+
+	#SSH clinet configuration
+	host*
+	indentifyfile ~/.ssh/school
+	passwordAuthorization no
+} 
